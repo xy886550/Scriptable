c441f52a191840e27ec6f9646a6a32753e4a5548
xy886550 <79785040+xy886550@users.noreply.github.com>
2021-09-27 15:03:40 +0800

    Add files via upload

---
+++ Pure Calendar.js
@@ -0,0 +1,253 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: red; icon-glyph: snowflake;
+/* ------------- 设置项目 ------------- */
+//功能对齐间距
+let widget = new ListWidget()
+widget.setPadding(12, 12, 12, 12) //上左下右
+const testMode = true  //是否预览 true false
+const widgetPreview = "Medium" //Small  Medium  large
+const showDlTq = false  //是否显示电量/天气
+const verticalAlignment = "bottom"  // 垂直对齐/top  middle  bottom
+const horizontalAlignment = "left"  // 水平对齐/left  center  right
+const elementSpacing = 2 //元素行间距
+refreshInterval = 10
+
+//时间字体显示设置
+const NowFont = "ChalkboardSE-Light"
+const NowSize = 35 //时间字体大小
+const NowColor = new Color("FFD700")
+const NowtextOpacity = 1 //文本不透明度
+const NowshadowColor = new Color("000000")
+const NowshadowRadius = 1
+const NowshadowOffset = new Point(0, 0)
+//日历字体显示设置
+const GlNlFont = ""
+const GlNlSize = 14
+const GlNlColor = new Color("ffffff")
+const GlNltextOpacity = 1
+const GlNlshadowColor = new Color("000000")
+const GlNlshadowRadius = 2
+const GlNlshadowOffset = new Point(0, 0)
+//电量天气字体显示设置
+const DlTqFont = ""
+const DlTqSize = 12
+const DlTqColor = new Color("ffffff")
+const DlTqtextOpacity = 2
+const DlTqshadowColor = new Color("000000")
+const DlTqshadowRadius = 1
+const DlTqshadowOffset = new Point(0, 0)
+
+//细AvenirNext-UltraLight HelveticaNeue-UltraLight
+//中AvenirNext-Regular Avenir-Light Arial-BoldMT Verdana ChalkboardSE-Light DBLCDTempBlack 
+//粗GillSans-UltraBold AvenirNext-Heavy Futura-CondensedExtraBold
+
+//黄色FFFF00 金黄FFD700 荧黄F0FF21 卡其F0E68C
+//橙色FFA500 珊瑚FF7F50 橙红FF4500 深粉FF1493
+//海绿20B2AA 浅绿90EE90 宝石66CDAA 黄绿9ACD32
+//水青00FFFF 粉蓝B0E0E6 淡蓝B8F3FF 菊蓝6495ED
+//薰衣E6E6FA 浅紫C4B8FF 板蓝7B68EE 巧克力D2691E
+
+const City = 'Yudu'
+const CityTQ = await getTQ()
+const TP = await getTP()
+const WNL = await getWNL()
+
+// 获取天气
+async function getTQ() {
+  const CityURI = encodeURI(`${City}`);
+  const url = `https://wttr.in/${CityURI}?format=%25c+%25f+%25m`;
+  const request = new Request(url)
+  const res = await request.loadString()
+  return res
+}
+
+// 获取背景图片
+async function getTP() {
+  let img_url_arr = [
+  // ✨博天·API  mobile:竖屏 pc:横屏
+  "https://api.btstu.cn/sjbz/api.php",
+  "https://api.btstu.cn/sjbz/api.php?method=pc&lx=meizi",
+  "https://api.btstu.cn/sjbz/api.php?method=pc&lx=dongman",
+  "https://api.btstu.cn/sjbz/api.php?method=pc&lx=fengjing",
+  // ✨Uomg·API img1:横屏  随机/美女/汽车/二次元/动漫
+  "https://api.uomg.com/api/rand.img1",
+  "https://api.uomg.com/api/rand.img1?sort=%E7%BE%8E%E5%A5%B3&format=images",
+  "https://api.uomg.com/api/rand.img1?sort=%E6%B1%BD%E8%BD%A6&format=images",
+  "https://api.uomg.com/api/rand.img1?sort=%E4%BA%8C%E6%AC%A1%E5%85%83&format=images",
+  "https://api.uomg.com/api/rand.img1?sort=%E5%8A%A8%E6%BC%AB&format=images",
+  // img2:竖屏  随机/美女/二次元/腿控
+  //"https://api.uomg.com/api/rand.img2",
+  //"https://api.uomg.com/api/rand.img2?sort=%E7%BE%8E%E5%A5%B3&format=images",
+  //"https://api.uomg.com/api/rand.img2?sort=%E4%BA%8C%E6%AC%A1%E5%85%83&format=images",
+  //"https://api.uomg.com/api/rand.img2?sort=%E8%85%BF%E6%8E%A7&format=images",
+  // avatar:头像  方形 女/男
+  "https://api.uomg.com/api/rand.avatar?sort=%E5%A5%B3&format=images",
+  // ✨小歪·API  横屏 风景
+  //"https://api.ixiaowai.cn/gqapi/gqapi.php",
+  // ✨动漫星空·API  横屏 ACG
+  //"https://api.dongmanxingkong.com/suijitupian/acg/1080p/index.php?",//失效
+  // ✨Loli·API  横屏 ACG
+  //"https://api.loli.bj/api",
+  // ✨接口互联·API  横竖屏
+  "https://api88.net/api/img/rand/",
+  //"https://api88.net/api/img/rand/?rand_type=rand_mz",//含动图
+  "https://img.tjit.net/bing/rand/"
+  // ✨MUZI API  横屏
+  //"https://api.womc.cn/api/znbz/znbz.php",
+  //"https://api.womc.cn/api/meinv/meinv.php",
+  //"https://api.womc.cn/api/ACG/api.php",
+  //"https://api.shaoyuu.com/duola/mu.php",
+  // ✨Bing API
+  //"https://area.sinaapp.com/bingImg/"
+  ]
+  const index = parseInt(Math.random() * img_url_arr.length)
+  let img_url = img_url_arr[index]
+  const req = new Request(img_url)
+  const img = await req.loadImage()
+  return {'img':img}
+}
+
+// 获取万年历
+async function getWNL() {
+  const url = "https://api.error.work/api/util/calendar"
+  const req = new Request(url)
+  const res = await req.loadJSON()
+  return res
+}
+
+// 获取公历信息
+const date = new Date()
+var weekInfo = date.getDay()
+var weekArr = ['周日', '周一', '周二', '周三', '周四', '周五', '周六'];
+let week = weekArr[weekInfo]
+const str = " " + date.getFullYear() + "年" + (date.getMonth() + 1) + "月" + date.getDate() + "日" + " " + week
+
+/* ------------ 初始化小组件 ------------ */
+
+// 先构建窗口小部件
+if (config.runsInWidget || testMode) {
+  widget.backgroundImage = await shadowImage(TP.img)
+
+if (verticalAlignment == "middle" || verticalAlignment == "bottom") {
+    widget.addSpacer()
+  }
+
+//时间 🕐
+let Now = widget.addDate(new Date())
+Now.date = new Date(new Date(new Date().toLocaleDateString()).getTime())
+Now.applyTimerStyle()
+if (NowFont == "" || NowFont == null) {
+  Now.font = Font.lightSystemFont(NowSize)
+  //light regular medium bold
+  } else {
+  Now.font = new Font(NowFont,NowSize)
+  }
+Now.textColor = NowColor
+Now.textOpacity = NowtextOpacity
+Now.shadowColor = NowshadowColor
+Now.shadowRadius = NowshadowRadius
+Now.shadowOffset = NowshadowOffset
+widget.addSpacer(elementSpacing)
+  
+//公历+农历/节日 📅
+let GlNl = widget.addText(str + '·' + `${WNL.data.nongli} ${WNL.data.jieri}`)
+if (GlNlFont == "" || GlNlFont == null) {
+  GlNl.font = Font.regularSystemFont(GlNlSize)
+  } else {
+  GlNl.font = new Font(GlNlFont,GlNlSize)
+  }
+GlNl.textColor = GlNlColor
+GlNl.textOpacity = GlNltextOpacity
+GlNl.shadowColor = GlNlshadowColor
+GlNl.shadowRadius = GlNlshadowRadius
+GlNl.shadowOffset = GlNlshadowOffset
+ 
+//电池/天气 🔋+☁️
+if (showDlTq) {
+  widget.addSpacer(elementSpacing)
+  let batteryinfo = getBatteryInfo()
+  let DlTq = widget.addText(batteryinfo + ' ｜ ' + `${CityTQ}`)
+  if (DlTqFont == "" || DlTqFont == null) {
+  DlTq.font = Font.regularSystemFont(DlTqSize)
+  } else {
+  DlTq.font = new Font(DlTqFont,DlTqSize)
+}
+DlTq.textColor = DlTqColor
+DlTq.textOpacity = DlTqtextOpacity
+DlTq.shadowColor = DlTqshadowColor
+DlTq.shadowRadius = DlTqshadowRadius
+DlTq.shadowOffset = DlTqshadowOffset
+}
+
+if (verticalAlignment == "top" || verticalAlignment == "middle") {
+  widget.addSpacer()
+  }
+
+Script.setWidget(widget)
+  if (testMode) {
+    let widgetSizeFormat = widgetPreview.toLowerCase()
+    if (widgetSizeFormat == "small")  { widget.presentSmall()  }
+    if (widgetSizeFormat == "medium") { widget.presentMedium() }
+    if (widgetSizeFormat == "large")  { widget.presentLarge()  }
+  }
+  Script.complete()
+}
+
+/* ------------- 辅助功能 ------------- */
+/**
+ * 判断是否在区域里
+ * @param {*} area 
+ * @param {*} value 
+ */
+function utilIsInArea(area, value) {
+  const matched = Object.keys(area).find(key => {
+    const [strat, end] = key.split('~')
+    return value > strat && value <= end
+  })
+  return area[matched]
+}
+
+/**
+ * 获取电池信息
+ */
+function getBatteryInfo() {
+  let batteryText = '🔋 '
+// 是否在充电
+  const isCharging = Device.isCharging()
+// 充电等级
+  const batteryLevel = Math.round(Device.batteryLevel() * 100)
+
+// 显示电量值
+  batteryText += `${batteryLevel}% `
+  
+// 电量状态提示语枚举
+  const batteryTextMap = {
+    '0~20': "🥶", 
+    '20~40': "🥺", 
+    '40~80': "😋", 
+    '80~100': "🥰"
+  }
+  if (isCharging) {
+    batteryText += batteryLevel < 95 ? "⚡️" : "🥵"
+  } else {
+    batteryText += utilIsInArea(batteryTextMap, batteryLevel)
+  }
+  return batteryText
+}
+
+/**
+   * 给图片加上半透明遮罩
+   * @param img 要处理的图片对象
+   * @return image
+   */
+async function shadowImage(img) {
+    let ctx = new DrawContext()
+    ctx.size = img.size
+    ctx.drawImageInRect(img, new Rect(0, 0, img.size['width'], img.size['height']))
+    // 图片遮罩颜色、透明度设置
+    ctx.setFillColor(new Color("#000000", 0.1))
+    ctx.fillRect(new Rect(0, 0, img.size['width'], img.size['height']))
+    let res = await ctx.getImage()
+    return res
+  }
--- 美图日历 3.js
+++ 美图日历 3.js
@@ -2,32 +2,90 @@
 // These must be at the very top of the file. Do not edit.
 // icon-color: pink; icon-glyph: images;
 /**
-* 版本: 2.0.0
-* 更新时间：2021.09.20
+* 版本: 3.0.0
+* 更新时间：2021.09.27
 */
-//############## 公共参数配置模块 ############## 
+//############## 参数配置模块 ##############
+
 const filename = `${Script.name()}.jpg`
 const files = FileManager.local()
 const path = files.joinPath(files.documentsDirectory(), filename)
-const changePicBg = false  //选择true时，使用透明背景
-const ImageMode = true   //选择true时，使用壁纸
-const previewSize =  (config.runsInWidget ? config.widgetFamily : "medium");// medium、small、large 预览大小
-const colorMode = false // 是否是纯色背景
+const changePicBg = false //是否使用透明背景
+const ImageMode = true    //是否使用壁纸
+const previewSize =  (config.runsInWidget ? config.widgetFamily : "medium");//预览大小 medium、small、large
+const colorMode = false   //是否使用纯色背景
 const COLOR_LIGHT_GRAY = new Color('#E5E7EB', 1);
 const COLOR_DARK_GRAY = new Color('#374151', 1);
 const COLOR_BAR_BACKGROUND = Color.dynamic(COLOR_LIGHT_GRAY, COLOR_DARK_GRAY);
 
-// 获取农历信息
-let date = new Date()
-const lunarInfo = await getLunar(date.getDate() - 1)
-let lunarJoinInfo = lunarInfo.lunarYearText+ " " + lunarInfo.infoLunarText + " " + lunarInfo.holidayText
+const verticalAlignment = "bottom"  //垂直对齐/top  middle  bottom
+const horizontalAlignment = "left"  //水平对齐/left  center  right
+const elementSpacing = 2  //元素行间距
+const showDlTq = false    //是否显示电量/天气
 
-// 获取星期
+// 时间字体显示设置
+const NowFont = "ChalkboardSE-Light"
+const NowSize = 35 //时间字体大小
+const NowColor = new Color("FFD700")
+const NowtextOpacity = 1 //文本不透明度
+const NowshadowColor = new Color("000000")
+const NowshadowRadius = 1
+const NowshadowOffset = new Point(0, 0)
+// 日历字体显示设置
+const GlNlFont = "" //为空则为默认
+const GlNlSize = 14
+const GlNlColor = new Color("ffffff")
+const GlNltextOpacity = 1
+const GlNlshadowColor = new Color("000000")
+const GlNlshadowRadius = 2
+const GlNlshadowOffset = new Point(0, 0)
+// 电量天气字体显示设置
+const DlTqFont = ""
+const DlTqSize = 12
+const DlTqColor = new Color("ffffff")
+const DlTqtextOpacity = 2
+const DlTqshadowColor = new Color("000000")
+const DlTqshadowRadius = 1
+const DlTqshadowOffset = new Point(0, 0)
+
+//细AvenirNext-UltraLight HelveticaNeue-UltraLight
+//中AvenirNext-Regular Avenir-Light Arial-BoldMT Verdana ChalkboardSE-Light DBLCDTempBlack 
+//粗GillSans-UltraBold AvenirNext-Heavy Futura-CondensedExtraBold
+
+//黄色FFFF00 金黄FFD700 荧黄F0FF21 卡其F0E68C
+//橙色FFA500 珊瑚FF7F50 橙红FF4500 深粉FF1493
+//海绿20B2AA 浅绿90EE90 宝石66CDAA 黄绿9ACD32
+//水青00FFFF 粉蓝B0E0E6 淡蓝B8F3FF 菊蓝6495ED
+//薰衣E6E6FA 浅紫C4B8FF 板蓝7B68EE 巧克力D2691E
+
+//############## 参数配置模块 ##############
+
+// 获取公历信息
+let date = new Date()
 var weekInfo = date.getDay()
 var weekArr = ['周日', '周一', '周二', '周三', '周四', '周五', '周六'];
 let week = weekArr[weekInfo]
-// 获取公历信息
-const str = date.getFullYear() + "年" + (date.getMonth() + 1) + "月" + date.getDate() + "日" + " " + week
+const str = " " + date.getFullYear() + "年" + (date.getMonth() + 1) + "月" + date.getDate() + "日" + " " + week
+
+// 获取农历信息
+const WNL = await getWNL()
+async function getWNL() {
+  const url = "https://api.error.work/api/util/calendar"
+  const req = new Request(url)
+  const res = await req.loadJSON()
+  return res
+}
+
+// 获取天气
+const City = 'Yudu'
+const CityTQ = await getTQ()
+async function getTQ() {
+  const CityURI = encodeURI(`${City}`);
+  const url = `https://wttr.in/${CityURI}?format=%25c+%25f+%25m`;
+  const request = new Request(url)
+  const res = await request.loadString()
+  return res
+}
 
 const padding = {
   top: 12,
@@ -37,7 +95,7 @@ const padding = {
 }
 let widget = await createWidget()
 
-//################ 透明背景模块 #################
+//################ 透明背景模块 ################
 
 if (!colorMode && !ImageMode && !config.runsInWidget && changePicBg) {
   const okTips = "您的小部件背景已准备就绪"
@@ -57,7 +115,7 @@ if (!colorMode && !ImageMode && !config.runsInWidget && changePicBg) {
     let shouldExit = await generateAlert(message, exitOptions)
     if (shouldExit) return
 
-    // Get screenshot and determine phone size.
+    // 获取屏幕截图并确定手机尺寸
     let img = await Photos.fromLibrary()
     let height = img.size.height
     let phone = phoneSizes()[height]
@@ -67,7 +125,7 @@ if (!colorMode && !ImageMode && !config.runsInWidget && changePicBg) {
       return
     }
 
-    // Prompt for widget size and position.
+    // 小部件大小和位置提示
     message = "您想要创建什么尺寸的小部件？"
     let sizes = ["小号", "中号", "大号"]
     let size = await generateAlert(message, sizes)
@@ -76,7 +134,7 @@ if (!colorMode && !ImageMode && !config.runsInWidget && changePicBg) {
     message = "您想它在什么位置？"
     message += (height == 1136 ? " (请注意，您的设备仅支持两行小部件，因此中间和底部选项相同。)" : "")
 
-    // Determine image crop based on phone size.
+    // 根据手机大小确定图像裁剪
     let crop = { w: "", h: "", x: "", y: "" }
     if (widgetSize == "小号") {
       crop.w = phone.小号
@@ -84,7 +142,7 @@ if (!colorMode && !ImageMode && !config.runsInWidget && changePicBg) {
       let positions = ["顶部 左边", "顶部 右边", "中间 左边", "中间 右边", "底部 左边", "底部 右边"]
       let position = await generateAlert(message, positions)
 
-      // Convert the two words into two keys for the phone size dictionary.
+      // 将这两个单词转换为手机大小字典的两个键
       let keys = positions[position].split(' ')
       crop.y = phone[keys[0]]
       crop.x = phone[keys[1]]
@@ -93,7 +151,7 @@ if (!colorMode && !ImageMode && !config.runsInWidget && changePicBg) {
       crop.w = phone.中号
       crop.h = phone.小号
 
-      // 中号 and 大号 widgets have a fixed x-value.
+      // 中号、大号部件有一个固定的x值
       crop.x = phone.左边
       let positions = ["顶部", "中间", "底部"]
       let position = await generateAlert(message, positions)
@@ -107,11 +165,11 @@ if (!colorMode && !ImageMode && !config.runsInWidget && changePicBg) {
       let positions = ["顶部", "底部"]
       let position = await generateAlert(message, positions)
 
-      // 大号 widgets at the 底部 have the "中间" y-value.
+      // 大号部件在底部时有"中间"值
       crop.y = position ? phone.中间 : phone.顶部
     }
 
-    // Crop image and finalize the widget.
+    // 裁剪图像并最终确定小部件
     let imgCrop = cropImage(img, new Rect(crop.x, crop.y, crop.w, crop.h))
 
     message = "您的小部件背景已准备就绪，退出到桌面预览。"
@@ -127,26 +185,25 @@ if (!colorMode && !ImageMode && !config.runsInWidget && changePicBg) {
 if (colorMode) {
   widget.backgroundColor = COLOR_BAR_BACKGROUND
 } else if (ImageMode) {
-  // 背景图
   let img_url_arr = [
   // ✨博天·API  mobile:竖屏 pc:横屏
   "https://api.btstu.cn/sjbz/api.php",
   "https://api.btstu.cn/sjbz/api.php?method=pc&lx=meizi",
   "https://api.btstu.cn/sjbz/api.php?method=pc&lx=dongman",
   "https://api.btstu.cn/sjbz/api.php?method=pc&lx=fengjing",
-  // ✨Uomg·API img1:横屏  随机/美女/汽车/二次元/动漫
-  "https://api.uomg.com/api/rand.img1",
-  "https://api.uomg.com/api/rand.img1?sort=%E7%BE%8E%E5%A5%B3&format=images",
-  "https://api.uomg.com/api/rand.img1?sort=%E6%B1%BD%E8%BD%A6&format=images",
-  "https://api.uomg.com/api/rand.img1?sort=%E4%BA%8C%E6%AC%A1%E5%85%83&format=images",
-  "https://api.uomg.com/api/rand.img1?sort=%E5%8A%A8%E6%BC%AB&format=images",
-  // img2:竖屏  随机/美女/二次元/腿控
-  //"https://api.uomg.com/api/rand.img2",
-  //"https://api.uomg.com/api/rand.img2?sort=%E7%BE%8E%E5%A5%B3&format=images",
-  //"https://api.uomg.com/api/rand.img2?sort=%E4%BA%8C%E6%AC%A1%E5%85%83&format=images",
-  //"https://api.uomg.com/api/rand.img2?sort=%E8%85%BF%E6%8E%A7&format=images",
-  // avatar:头像  方形 女/男
-  "https://api.uomg.com/api/rand.avatar?sort=%E5%A5%B3&format=images",
+  // ✨Uomg·API  img1:横屏 img2:竖屏 avatar:头像
+  "https://api.uomg.com/api/rand.img1",//随机
+  "https://api.uomg.com/api/rand.img1?sort=%E7%BE%8E%E5%A5%B3&format=images",//美女
+  "https://api.uomg.com/api/rand.img1?sort=%E6%B1%BD%E8%BD%A6&format=images",//汽车
+  "https://api.uomg.com/api/rand.img1?sort=%E4%BA%8C%E6%AC%A1%E5%85%83&format=images",//ACG
+  "https://api.uomg.com/api/rand.img1?sort=%E5%8A%A8%E6%BC%AB&format=images",//动漫
+
+  //"https://api.uomg.com/api/rand.img2",//随机
+  //"https://api.uomg.com/api/rand.img2?sort=%E7%BE%8E%E5%A5%B3&format=images",//美女
+  //"https://api.uomg.com/api/rand.img2?sort=%E4%BA%8C%E6%AC%A1%E5%85%83&format=images",//ACG
+  //"https://api.uomg.com/api/rand.img2?sort=%E8%85%BF%E6%8E%A7&format=images",//腿控
+
+  "https://api.uomg.com/api/rand.avatar?sort=%E5%A5%B3&format=images",//头像
   // ✨小歪·API  横屏 风景
   //"https://api.ixiaowai.cn/gqapi/gqapi.php",
   // ✨动漫星空·API  横屏 ACG
@@ -155,7 +212,7 @@ if (colorMode) {
   //"https://api.loli.bj/api",
   // ✨接口互联·API  横竖屏
   "https://api88.net/api/img/rand/",
-  //"https://api88.net/api/img/rand/?rand_type=rand_mz",//含动图
+  "https://api88.net/api/img/rand/?rand_type=rand_mz",//含动图
   "https://img.tjit.net/bing/rand/"
   // ✨MUZI API  横屏
   //"https://api.womc.cn/api/znbz/znbz.php",
@@ -164,17 +221,17 @@ if (colorMode) {
   //"https://api.shaoyuu.com/duola/mu.php",
   // ✨Bing API
   //"https://area.sinaapp.com/bingImg/"
-    ]
-    const index = parseInt(Math.random() * img_url_arr.length)
-    let img_url = img_url_arr[index]
-    const img = await getImageByUrl(img_url, `x.xiāo`, false)
-   widget.backgroundImage = await shadowImage(img)
-}
-else {
-  widget.backgroundImage = files.readImage(path)
+  ]
+  const index = parseInt(Math.random() * img_url_arr.length)
+  let img_url = img_url_arr[index]
+  const img = await getImageByUrl(img_url, `x.xiāo`, false)
+  widget.backgroundImage = await shadowImage(img)
+  }
+  else {
+   widget.backgroundImage = files.readImage(path)
 }
 // 设置边距(上，左，下，右)
-  widget.setPadding(padding.top, padding.left, padding.bottom, padding.right)
+widget.setPadding(padding.top, padding.left, padding.bottom, padding.right)
 // 设置组件
 if (!config.runsInWidget) {
   switch (previewSize) {
@@ -189,104 +246,104 @@ if (!config.runsInWidget) {
       break;
   }
 }
-Script.setWidget(widget)
-// 完成脚本
-Script.complete()
-// 预览
+Script.setWidget(widget)// 完成脚本
+Script.complete()// 预览
 
 //############### 创建小组件内容 ###############
 
 async function createWidget() {
   let widget = new ListWidget()
 
-//写入当前时间
-  widget.addSpacer()
+  if (verticalAlignment == "middle" || verticalAlignment == "bottom") {
+    widget.addSpacer()
+  }
+  // 时间 🕐
   let Now = widget.addDate(new Date())
   Now.date = new Date(new Date(new Date().toLocaleDateString()).getTime())
   Now.applyTimerStyle()
-  Now.font = new Font('AvenirNext-Heavy',30)
-//细AvenirNext-UltraLight HelveticaNeue-UltraLight
-//中Avenir-Light AvenirNext-Regular Arial-BoldMT Verdana ChalkboardSE-Light DBLCDTempBlack 
-//粗GillSans-UltraBold AvenirNext-Heavy Futura-CondensedExtraBold
-  Now.textColor = new Color("ffffff")
-//白ffffff 黄ffcd00 荧绿f0ff21 淡蓝B8F3FF 浅紫C4B8FF
-  Now.textOpacity = 1 //文本不透明度
-  Now.leftAlignText() //center right
-  Now.shadowColor = new Color("000000") //阴影颜色
-  Now.shadowRadius = 1  //阴影半径
-  Now.shadowOffset = new Point(0, 0) //阴影偏移
-  //widget.addSpacer()
+  if (NowFont == "" || NowFont == null) {
+   Now.font = Font.lightSystemFont(NowSize)
+    //light regular medium bold
+  } else {
+   Now.font = new Font(NowFont,NowSize)
+  }
+  Now.textColor = NowColor
+  Now.textOpacity = NowtextOpacity
+  Now.shadowColor = NowshadowColor
+  Now.shadowRadius = NowshadowRadius
+  Now.shadowOffset = NowshadowOffset
+  widget.addSpacer(elementSpacing)
   
-//写入公历/农历/节日
-  //widget.addSpacer()
-  let full = widget.addText(str + '·' + `${lunarJoinInfo}`)
-  full.font = Font.lightSystemFont(14)//light  medium
-  //full.font = new Font('Avenir-Light',14)
-  full.textColor = new Color("ffffff")
-  full.textOpacity = 1 //文本不透明度
-  full.leftAlignText()
-  full.shadowColor = new Color("000000") //阴影颜色
-  full.shadowRadius = 2  //阴影半径
-  full.shadowOffset = new Point(0, 0) //阴影偏移
-  //widget.addSpacer()
-        
-  return widget
- }
-
-//获取农历时间
-// async function getLunarData() {
-//   const url = 'https://api.xlongwei.com/service/datetime/convert.json'
-//   const request = new Request(url)
-//   const res = await request.loadJSON()
-//   return `${res.ganzhi}${res.shengxiao}年 农历${res.chinese.replace(/.*年/, '')}`
-// }
-
-async function getLunar(day) {
-  // 缓存key
-  const cacheKey = "NK-lunar-cache"
-  // 万年历数据
-  let response = undefined
-  try {
-    const request = new Request("https://wannianrili.51240.com/")
-    const defaultHeaders = {
-      "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36"
-    }
-    request.method = 'GET'
-    request.headers = defaultHeaders
-    const html = await request.loadString()
-    let webview = new WebView()
-    await webview.loadHTML(html)
-    var getData = `
-              function getData() {
-                  try {
-                      infoLunarText = document.querySelector('div#wnrl_k_you_id_${day}.wnrl_k_you .wnrl_k_you_id_wnrl_nongli').innerText
-                      holidayText = document.querySelectorAll('div.wnrl_k_zuo div.wnrl_riqi')[${day}].querySelector('.wnrl_td_bzl').innerText
-                      lunarYearText = document.querySelector('div.wnrl_k_you_id_wnrl_nongli_ganzhi').innerText
-                      lunarYearText = lunarYearText.slice(0, lunarYearText.indexOf('年')+1)
-                      if(infoLunarText.search(holidayText) != -1) {
-                          holidayText = ''
-                      }
-                  } catch {
-                      holidayText = ''
-                  }
-                  return {infoLunarText: infoLunarText,  lunarYearText: lunarYearText,  holidayText: holidayText }
-              }
-              
-              getData()`
-
-    // 节日数据  
-    response = await webview.evaluateJavaScript(getData, false)
-    Keychain.set(cacheKey, JSON.stringify(response))
-    console.log(`[+]农历输出：${JSON.stringify(response)}`);
-  } catch (e) {
-    console.error(`[+]农历请求出错：${e}`)
-    if (Keychain.contains(cacheKey)) {
-      const cache = Keychain.get(cacheKey)
-      response = JSON.parse(cache)
-    }
+  // 公历+农历/节日 📅
+  let GlNl = widget.addText(str + '·' + `${WNL.data.nongli} ${WNL.data.jieri}`)
+  if (GlNlFont == "" || GlNlFont == null) {
+   GlNl.font = Font.regularSystemFont(GlNlSize)
+  } else {
+   GlNl.font = new Font(GlNlFont,GlNlSize)
+  }
+  GlNl.textColor = GlNlColor
+  GlNl.textOpacity = GlNltextOpacity
+  GlNl.shadowColor = GlNlshadowColor
+  GlNl.shadowRadius = GlNlshadowRadius
+  GlNl.shadowOffset = GlNlshadowOffset
+ 
+  // 电池/天气 🔋+☁️
+  if (showDlTq) {
+   widget.addSpacer(elementSpacing)
+  let batteryinfo = getBatteryInfo()
+  let DlTq = widget.addText(batteryinfo + ' ｜ ' + `${CityTQ}`)
+   if (DlTqFont == "" || DlTqFont == null) {
+   DlTq.font = Font.regularSystemFont(DlTqSize)
+   } else {
+   DlTq.font = new Font(DlTqFont,DlTqSize)
+   }
+  DlTq.textColor = DlTqColor
+  DlTq.textOpacity = DlTqtextOpacity
+  DlTq.shadowColor = DlTqshadowColor
+  DlTq.shadowRadius = DlTqshadowRadius
+  DlTq.shadowOffset = DlTqshadowOffset
   }
 
-  return response
+  if (verticalAlignment == "top" || verticalAlignment == "middle") {
+  widget.addSpacer()
+  }
+  
+  return widget
+}
+
+//############ 获取电量信息部分 ############
+
+// 判断是否在区域里
+function utilIsInArea(area, value) {
+  const matched = Object.keys(area).find(key => {
+    const [strat, end] = key.split('~')
+    return value > strat && value <= end
+  })
+  return area[matched]
+}
+
+// 获取电池信息
+function getBatteryInfo() {
+  let batteryText = '🔋 '
+  // 是否在充电
+  const isCharging = Device.isCharging()
+  // 充电等级
+  const batteryLevel = Math.round(Device.batteryLevel() * 100)
+  // 显示电量值
+  batteryText += `${batteryLevel}% `
+  // 电量状态提示语枚举
+  const batteryTextMap = {
+    '0~20': "🥶", 
+    '20~40': "🥺", 
+    '40~80': "😋", 
+    '80~100': "🥰"
+  }
+  if (isCharging) {
+    batteryText += batteryLevel < 95 ? "⚡️" : "🥵"
+  } else {
+    batteryText += utilIsInArea(batteryTextMap, batteryLevel)
+  }
+  return batteryText
 }
 
 //############ 背景模块-逻辑处理部分 ############
@@ -318,14 +375,13 @@ async function generateAlert(message, options) {
   return response
 }
 
-// Crop an image into the specified rect.
+// 将图像裁剪到指定的矩形中
 function cropImage(img, rect) {
   let draw = new DrawContext()
   draw.size = new Size(rect.width, rect.height)
   draw.drawImageAtPoint(img, new Point(-rect.x, -rect.y))
   return draw.getImage()
 }
-
 
 async function getImageByUrl(url, cacheKey, useCache = true) {
   const cacheFile = FileManager.local().joinPath(FileManager.local().temporaryDirectory(), cacheKey)
@@ -354,7 +410,7 @@ async function getImageByUrl(url, cacheKey, useCache = true) {
   }
 }
 
-// Pixel sizes and positions for widgets on all supported phones.
+// 所有受支持手机上小部件的像素大小和位置
 function phoneSizes() {
   let phones = {
     "2340": { // 12mini
