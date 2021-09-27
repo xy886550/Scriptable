23ab9bac006a4bb301dec22e6898e00a0e62891d
xy886550 <79785040+xy886550@users.noreply.github.com>
2021-09-27 15:49:35 +0800

    Update 透明组件日历版.js

--- 透明组件日历版.js
+++ 透明组件日历版.js
@@ -1,9 +1,6 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: light-gray; icon-glyph: calendar-times;
-// Variables used by Scriptable.
-// These must be at the very top of the file. Do not edit.
-// icon-color: deep-purple; icon-glyph: image;
 
 // This widget was created by Max Zeryck @mzeryck
 
